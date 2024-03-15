14e18647c34c3c600117ccf0fbd89b0ca7e4a1a2
Tauter <harangi.ftam@gmail.com>
2024-03-15 18:09:09 +0100

    init

---
+++ src/main/java/com/bredex/test/BredexTestApplication.java
@@ -0,0 +1,13 @@
+package com.bredex.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BredexTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BredexTestApplication.class, args);
+    }
+
+}
