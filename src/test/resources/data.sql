2266898f73afe64dba38e97e7f07fd6ba62a5f33
Tauter <harangi.ftam@gmail.com>
2024-03-17 19:34:28 +0100

    Ad test data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO user_account(user_name, email, password)
 VALUES ('testName', 'test@email.com', 'testPassword');
+
+
+INSERT INTO ad(user_account_id, brand, type, description)
+VALUES (1, 'Skoda', 'Superb', '2.0 TDI');
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE IF NOT EXISTS user_account
     password  varchar(255) not null,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS ad
+(
+    id              int auto_increment,
+    user_account_id int          not null,
+    brand           varchar(20)  not null,
+    type            varchar(20)  not null,
+    description     varchar(200) not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_account_id) REFERENCES user_account (id)
+)
