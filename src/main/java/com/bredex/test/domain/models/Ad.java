cbdcd752c475a1f523d048b87eac3a5c257312d1
Tauter <harangi.ftam@gmail.com>
2024-03-17 19:34:19 +0100

    Ad model missing annotation

--- src/main/java/com/bredex/test/domain/models/Ad.java
+++ src/main/java/com/bredex/test/domain/models/Ad.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Builder
 @Entity
@@ -17,6 +15,7 @@ import javax.persistence.ManyToOne;
 public class Ad {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @ManyToOne
