d1ba779fa7f22f0e6696a14bd4acb86c9c41141d
Tauter <harangi.ftam@gmail.com>
2024-03-17 17:12:40 +0100

    AuthControllerTest init

---
+++ src/test/java/com/bredex/test/web/controllers/AuthControllerTest.java
@@ -0,0 +1,51 @@
+package com.bredex.test.web.controllers;
+
+import com.bredex.test.domain.models.UserAccount;
+import com.bredex.test.domain.repositories.IUserAccountRepository;
+import com.bredex.test.web.dtos.RegistrationDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class AuthControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private IUserAccountRepository repository;
+
+    @Test
+    void signUp() {
+
+        int userAccountCount = this.repository.findAll().size();
+
+        RegistrationDto body = RegistrationDto.builder()
+                .email("brandNewEmail")
+                .name("brandNewName")
+                .password("brandNewEmail")
+                .build();
+
+        this.webTestClient.post()
+                .uri("/auth/signup")
+                .bodyValue(body)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.CREATED)
+                .expectBody().isEmpty();
+
+        Assertions.assertEquals(userAccountCount + 1, this.repository.findAll().size());
+
+        UserAccount savedUser = this.repository.findAll().get(userAccountCount);
+
+        Assertions.assertEquals(body.getEmail(), savedUser.getEmail());
+        Assertions.assertEquals(body.getName(), savedUser.getUserName());
+        Assertions.assertEquals(body.getPassword(), savedUser.getPassword());
+        Assertions.assertNotNull(savedUser.getId());
+    }
+}
