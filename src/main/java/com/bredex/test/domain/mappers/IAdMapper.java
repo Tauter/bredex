91167e8710fead859c25fea530b1376961b39faa
Tauter <harangi.ftam@gmail.com>
2024-03-18 11:47:25 +0100

    adMapper new method, test

--- src/main/java/com/bredex/test/domain/mappers/IAdMapper.java
+++ src/main/java/com/bredex/test/domain/mappers/IAdMapper.java
@@ -3,17 +3,16 @@ package com.bredex.test.domain.mappers;
 import com.bredex.test.domain.models.Ad;
 import com.bredex.test.domain.models.UserAccount;
 import com.bredex.test.web.dtos.AdCreationDto;
+import com.bredex.test.web.dtos.AdResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IAdMapper {
 
-    @Mapping(target = "brand", source = "dto.brand")
-    @Mapping(target = "type", source = "dto.type")
-    @Mapping(target = "description", source = "dto.description")
-    @Mapping(target = "price", source = "dto.price")
-    @Mapping(target = "userAccount", source = "userAccount")
     @Mapping(target = "id", ignore = true)
     Ad mapFromCreationDto(AdCreationDto dto, UserAccount userAccount);
+
+    @Mapping(target = "userAccountId", source = "ad.userAccount.id")
+    AdResponseDto mapToResponse(Ad ad);
 }
--- src/test/java/com/bredex/test/domain/mappers/IAdMapperTest.java
+++ src/test/java/com/bredex/test/domain/mappers/IAdMapperTest.java
@@ -3,6 +3,7 @@ package com.bredex.test.domain.mappers;
 import com.bredex.test.domain.models.Ad;
 import com.bredex.test.domain.models.UserAccount;
 import com.bredex.test.web.dtos.AdCreationDto;
+import com.bredex.test.web.dtos.AdResponseDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +12,7 @@ class IAdMapperTest {
     private final IAdMapper mapper = new IAdMapperImpl();
 
     @Test
-    void mapTo() {
+    void mapFromCreation() {
         UserAccount user = UserAccount.builder().id(1L).build();
 
         AdCreationDto data = AdCreationDto.builder().brand("newBrand").type("newType").description("newDescription").price(1L).build();
@@ -19,6 +20,19 @@ class IAdMapperTest {
         Ad expected = Ad.builder().id(null).userAccount(user).price(1L).brand("newBrand").type("newType").description("newDescription").build();
 
         Ad result = this.mapper.mapFromCreationDto(data, user);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void mapToResponse() {
+        UserAccount user = UserAccount.builder().id(2L).build();
+
+        Ad data = Ad.builder().brand("newBrand").type("newType").description("newDescription").price(1L).id(1L).userAccount(user).build();
+
+        AdResponseDto expected = AdResponseDto.builder().id(1L).userAccountId(2L).price(1L).brand("newBrand").type("newType").description("newDescription").build();
+
+        AdResponseDto result = this.mapper.mapToResponse(data);
 
         Assertions.assertEquals(expected, result);
     }
