9908b7ae77d2468926b9e85b3899fcc9f372e97c
Tauter <harangi.ftam@gmail.com>
2024-03-18 17:40:07 +0100

    added missing annotation

--- src/main/java/com/bredex/test/web/errors/UserAlreadyExistException.java
+++ src/main/java/com/bredex/test/web/errors/UserAlreadyExistException.java
@@ -2,7 +2,7 @@ package com.bredex.test.web.errors;
 
 public class UserAlreadyExistException extends Exception implements ICustomException {
 
-
+    @Override
     public ClassException of(String email) {
         return new ClassException("UserAccount", "User already exist with this email: " + email);
     }
--- src/main/java/com/bredex/test/web/errors/UserNotFoundException.java
+++ src/main/java/com/bredex/test/web/errors/UserNotFoundException.java
@@ -3,6 +3,7 @@ package com.bredex.test.web.errors;
 public class UserNotFoundException extends Exception implements ICustomException {
 
 
+    @Override
     public ClassException of(String email) {
         return new ClassException("UserAccount","User not found with this email: " + email);
     }
