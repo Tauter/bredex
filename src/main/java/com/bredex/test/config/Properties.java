2012ed973f06f75876095de73b4d9468fc667cc7
Tauter <harangi.ftam@gmail.com>
2024-03-18 11:38:37 +0100

    application.properties model

---
+++ src/main/java/com/bredex/test/config/Properties.java
@@ -0,0 +1,12 @@
+package com.bredex.test.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class Properties {
+    @Value("${url}")
+    private String defaultUrl;
+}
