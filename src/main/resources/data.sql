eda0011c13cb8faad0e8b93f5a9c86f9b703a254
Tauter <harangi.ftam@gmail.com>
2024-03-18 13:12:00 +0100

    application.properties refactor

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,10 @@ VALUES ('user', 'email@gmail.com', 'password');
 
 
 INSERT INTO ad(user_account_id, brand, type, description, price)
-VALUES (1, 'Skoda', 'Superb', '2.0 TDI',1111);
+VALUES (1, 'Skoda', 'Superb', '2.0 TDI', 1),
+       (1, 'Skoda', 'Fabia', '1.0 TDI', 2),
+       (1, 'Audi', 'A4', '1.9 PD TDI', 1),
+       (1, 'Audi', 'A6', '2.0 PD TDI', 3),
+       (1, 'Volvo', 'S60', '1.6', 4),
+       (1, 'Volvo', 'S80', '1.8', 5),
+       (1, 'Opel', 'Corsa', '1.4', 5);
--- src/test/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO user_account(user_name, email, password)
-VALUES ('testName', 'test@email.com', 'testPassword');
-
-
-INSERT INTO ad(user_account_id, brand, type, description, price)
-VALUES (1, 'Skoda', 'Superb', '2.0 TDI',1111);
