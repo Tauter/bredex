6afe8f8a85c71d2a963f994017f05e1de1778940
Tauter <harangi.ftam@gmail.com>
2024-03-18 11:38:06 +0100

    adRepository added search query

--- src/main/java/com/bredex/test/domain/repositories/IAdRepository.java
+++ src/main/java/com/bredex/test/domain/repositories/IAdRepository.java
@@ -1,9 +1,24 @@
 package com.bredex.test.domain.repositories;
 
 import com.bredex.test.domain.models.Ad;
+import com.bredex.test.web.dtos.SearchRequestDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IAdRepository extends JpaRepository<Ad, Long> {
+
+    @Query("SELECT a.* " +
+            "FROM Ad a " +
+            "WHERE " +
+            "a.brand like '%:#{#searchTerm.brand}%' " +
+            "OR " +
+            "a.type like '%:#{#searchTerm.type}%' " +
+            "OR " +
+            "a.price = :#{#searchTerm.price} "
+    )
+    List<Ad> search(SearchRequestDto searchTerm);
 }
