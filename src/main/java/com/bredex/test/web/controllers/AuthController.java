250cd40b993210df8d105e33162b48eb47595e06
Tauter <harangi.ftam@gmail.com>
2024-03-19 20:44:05 +0100

    AuthController

--- src/main/java/com/bredex/test/web/controllers/AuthController.java
+++ src/main/java/com/bredex/test/web/controllers/AuthController.java
@@ -3,6 +3,8 @@ package com.bredex.test.web.controllers;
 import com.bredex.test.domain.mappers.IUserAccountMapper;
 import com.bredex.test.domain.models.UserAccount;
 import com.bredex.test.services.IUserAccountService;
+import com.bredex.test.services.auth.ILogOutService;
+import com.bredex.test.services.auth.JwtUtilsService;
 import com.bredex.test.services.validators.RegistrationDtoValidator;
 import com.bredex.test.web.dtos.JwtResponseDto;
 import com.bredex.test.web.dtos.RegistrationDto;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +29,10 @@ public class AuthController {
     private final IUserAccountMapper userAccountMapper;
 
     private final RegistrationDtoValidator registrationDtoValidator;
+
+    private final JwtUtilsService jwtUtilsService;
+
+    private final ILogOutService logOutService;
 
 
     @PostMapping("/signup")
@@ -52,13 +59,30 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponseDto> login(@RequestParam String email, @RequestParam String password) {
+        Optional<UserAccount> foundEmail = this.userAccountService.findByEmail(email);
 
-        return ResponseEntity.ok(JwtResponseDto.builder().build());
+        if (foundEmail.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        if (!foundEmail.get().getPassword().equals(password))
+            return ResponseEntity.notFound().build();
+
+        JwtResponseDto accessTokens = this.jwtUtilsService.generate(foundEmail.get());
+
+        return ResponseEntity.ok(accessTokens);
 
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout() {
+    public ResponseEntity<Void> logout(Principal principal) {
+
+        Optional<UserAccount> userAccount = this.userAccountService.findByEmail(principal.getName());
+
+        if (userAccount.isEmpty())
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        this.logOutService.logout(userAccount.get());
+
         return ResponseEntity.ok().build();
     }
 }
